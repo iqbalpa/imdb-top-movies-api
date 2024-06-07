6bed01e52adc3589e63e412eca21d41c9885a1b4
iqbalpa <iqbalpahlevi16@gmail.com>
2024-06-07 22:19:53 +0700

    feat: implement get all and get by title

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Body } from '@nestjs/common';
+import { Movie } from '@prisma/client';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  constructor(private readonly movieService: MoviesService) {}
+
+  @Get()
+  async findAll(): Promise<Movie[]> {
+    return this.movieService.findAll();
+  }
+
+  @Get(':title')
+  async findOne(@Body('title') title: string): Promise<Movie> {
+    return this.movieService.findOne(title);
+  }
+}
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import prisma from 'src/utils/prisma';
+import { Movie } from '@prisma/client';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  async findAll(): Promise<Movie[]> {
+    const movies: Movie[] = await prisma.movie.findMany({});
+    return movies;
+  }
+
+  async findOne(title: string): Promise<Movie> {
+    const movie: Movie = await prisma.movie.findUnique({
+      where: { SeriesTitle: title },
+    });
+    return movie;
+  }
+}
