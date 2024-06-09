0f448d11062e6f11c852b4c521417363f9c0c922
iqbalpa <iqbalpahlevi16@gmail.com>
2024-06-09 14:30:37 +0700

    ci: docker setup finish

--- docker.sh
+++ docker.sh
@@ -1,10 +1,11 @@
 ## build docker images
 echo "docker build"
-docker build -t imdb-api .
+docker build -t imdb-api:v2 .
 
 ## run docker container
 # nestjs app listen to port 3000
 # docker expose port 3000
 # localhost mapping port 8080 to port 3000 
 # check your app in localhost:8080
-docker run -d --name imdb-api --restart=always -p 8080:3000 imdbi-api:latest
+echo "docker container run"
+docker run -d --name imdb-api-v2 --restart=always -p 8080:3000 imdbi-ap:v2
